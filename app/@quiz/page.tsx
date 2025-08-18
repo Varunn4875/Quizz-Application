601e9e1c4dcfc618c2ea7dc6473bb3a3e84a60f8
Varun Selvakumar <varunselvakumar6@gmail.com>
2025-08-18 21:41:13 +0530

    Update page.tsx

--- app/@quiz/page.tsx
+++ app/@quiz/page.tsx
@@ -13,12 +13,13 @@ function Quiz() {
     useEffect(()=>{
         async function getQuestions() {
             setLoading(true)
+
             const {results} =await(
             await fetch(`https://opentdb.com/api.php?amount=${config.numberOfQuestion}&category=${config.category.id}&difficulty=${config.level}&type=${config.type}`)).json()
             let shuffledResults=results.map((e)=>{
                 let value=[...e.incorrect_answers,e.correct_answer]
-                .map(value=>({value,sort:Math.random()}))
-                .sort((a,b)=>a.sort-b.sort)  
+                .map(value=>({value,rand:Math.random()}))
+                .sort((a,b)=>a.rand-b.rand)  
                 .map(({value})=>value)
                 e.answers=[...value]
                 return e;
@@ -39,10 +40,11 @@ setLoading(false)
             <h4 className="mb-4 text-2xl font-bold leading-none tracking-tight text-blue-900 md:text-5xl lg:text-6xl dark:text-white"> { questions.length?questions[0].question:'no questions'}
             </h4>
         <div className="flex justify-evenly items-center my-20 flex-wrap w-[90%]">
-            <button type="button" className=" w-[33%] my-4 py-3.5 px-5 me-2 mb-2 text-lg shadow-2xl font-medium text-gray-900 focus:outline-none bg-white rounded-lg  hover:bg-blue-600 hover:text-gray-100 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Alternative</button>
-            <button type="button" className=" w-[33%] my-4 py-3.5 px-5 me-2 mb-2 text-lg shadow-2xl font-medium text-gray-900 focus:outline-none bg-white rounded-lg  hover:bg-blue-600 hover:text-gray-100 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Alternative</button>
-            <button type="button" className="w-[33%] my-4 py-3.5 px-5 me-2 mb-2 text-lg shadow-2xl font-medium text-gray-900 focus:outline-none bg-white rounded-lg    hover:bg-blue-600 hover:text-gray-100 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Alternative</button>
-            <button type="button" className="w-[33%] my-4 py-3.5 px-5 me-2 mb-2 text-lg shadow-2xl font-medium text-gray-900 focus:outline-none bg-white rounded-lg    hover:bg-blue-600 hover:text-gray-100 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Alternative</button>
+            <button type="button" className=" w-[33%] my-4 py-3.5 px-5 me-2 mb-2 text-lg shadow-2xl font-medium text-gray-900 focus:outline-none bg-white rounded-lg  hover:bg-blue-600 hover:text-gray-100 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white 
+            dark:hover:bg-gray-700">option 1</button>
+            <button type="button" className=" w-[33%] my-4 py-3.5 px-5 me-2 mb-2 text-lg shadow-2xl font-medium text-gray-900 focus:outline-none bg-white rounded-lg  hover:bg-blue-600 hover:text-gray-100 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">option 2</button>
+            <button type="button" className="w-[33%] my-4 py-3.5 px-5 me-2 mb-2 text-lg shadow-2xl font-medium text-gray-900 focus:outline-none bg-white rounded-lg    hover:bg-blue-600 hover:text-gray-100 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">option 3</button>
+            <button type="button" className="w-[33%] my-4 py-3.5 px-5 me-2 mb-2 text-lg shadow-2xl font-medium text-gray-900 focus:outline-none bg-white rounded-lg    hover:bg-blue-600 hover:text-gray-100 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">option 4</button>
         </div>
         <button type="button" className=" w-[33%] my-4 py-3.5 px-5 me-2 mb-2 text-lg shadow-2xl font-medium text-gray-900 focus:outline-none bg-white rounded-lg  hover:bg-blue-900 hover:text-gray-100 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Next</button>
         </section>
